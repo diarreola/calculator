66872ceeaad93de941f212b77b4f391a2708f1e9
Diana Arreola <arreoladiana11@gmail.com>
2022-06-14 18:48:31 -0700

    add AC function

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButtons = document.querySelector('[data-delete]')
-const allClearButtons = document.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -93,5 +93,10 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
   calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
   calculator.updateDisplay()
 })
